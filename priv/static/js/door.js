636515ad1e15f3beb51e5e7a7cd3b06f8e22deae
Jose Rodriguez <jose.rodriguez@intuitivo.com>
2023-08-10 13:03:49 -0300

    wait pipeline ready

--- priv/static/js/door.js
+++ priv/static/js/door.js
@@ -16,11 +16,11 @@
 
   setTimeout(() => initStream(), 1000)
 
-  setTimeout(() => changeVideo("0", 1), 2000)
+  setTimeout(() => changeVideo("0", 1), 2500)
 
-  setTimeout(() => changeVideo("1", 1), 2000)
+  setTimeout(() => changeVideo("1", 1), 2500)
   
-  setTimeout(() => changeVideo("2", 1), 2000)
+  setTimeout(() => changeVideo("2", 1), 2500)
 
   async function fetchBinaryData(url, data) {
     const response = await fetch(url, {
