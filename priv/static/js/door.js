3e16a217c7d32ffbe97c38a4392559da3a7b16b7
Jose Rodriguez <jose.rodriguez@intuitivo.com>
2023-08-10 18:49:11 -0300

    fix to hw_check

--- priv/static/js/door.js
+++ priv/static/js/door.js
@@ -16,11 +16,11 @@
 
   setTimeout(() => initStream(), 1000)
 
-  setTimeout(() => changeVideo("0", 1), 2500)
+  setTimeout(() => changeVideo("0", 1), 2000)
 
-  setTimeout(() => changeVideo("1", 1), 2500)
+  setTimeout(() => changeVideo("1", 1), 2000)
   
-  setTimeout(() => changeVideo("2", 1), 2500)
+  setTimeout(() => changeVideo("2", 1), 2000)
 
   async function fetchBinaryData(url, data) {
     const response = await fetch(url, {
@@ -36,6 +36,7 @@
     }
   
     return response.arrayBuffer();
+    
   }
 
   function arrayBufferToBase64(buffer) {
@@ -81,15 +82,13 @@
     const cam = document.querySelector(`#cam${cam_index}`);
     
     const format_index = index.toString().padStart(4, '0');
-
-    //cam.setAttribute('src', `/root/cam${cam_index}/frame${format_index}.jpg`)
     
     fetchBinaryData("/api/v1/cam", {cam_index: cam_index, format_index: format_index})
   .then((binaryData) => {
     setcam(cam, binaryData);
   })
   .catch((error) => {
-    console.error('Error:', error);
+    console.error('Error: ', error);
   });
    
     await sleep(1000)
